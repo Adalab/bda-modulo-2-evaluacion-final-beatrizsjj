44f938d217bb11604ddd9cf58bd0411e60562432
Beatriz San José <beatrizsanjose.j@gmail.com>
2026-05-18 19:02:26 +0200

    Consultas ejercicio 2

--- ejercicio-2-sakila.sql
+++ ejercicio-2-sakila.sql
@@ -43,3 +43,49 @@ FROM film
 WHERE rating != "R" AND rating != "PG-13";
 
 -- Encuentra la cantidad total de películas en cada clasificación de la tabla film y muestra la clasificación junto con el recuento.
+SELECT DISTINCT rating, COUNT(*) AS total_film_per_rating
+FROM film
+GROUP BY rating;
+
+-- Encuentra la cantidad total de películas alquiladas por cada cliente y muestra el ID del cliente, su
+-- nombre y apellido junto con la cantidad de películas alquiladas.
+
+SELECT c.customer_id, c.first_name, c.last_name, COUNT(*) AS films_per_customer
+FROM customer c
+LEFT JOIN rental r ON r.customer_id= c.customer_id
+GROUP BY c.customer_id;
+
+
+-- Encuentra la cantidad total de películas alquiladas por categoría y muestra el nombre de la categoría 
+-- junto con el recuento de alquileres.
+SELECT c.name, COUNT(rental_id) AS total_rental
+FROM category c
+LEFT JOIN film_category fc ON c.category_id = fc.category_id
+LEFT JOIN film f ON f.film_id = fc.film_id
+LEFT JOIN inventory i ON f.film_id = i.film_id
+LEFT JOIN rental r ON r.inventory_id = i.inventory_id
+GROUP BY c.name;
+
+-- Encuentra el promedio de duración de las películas para cada clasificación de la tabla film y
+-- muestra la clasificación junto con el promedio de duración.
+SELECT rating, AVG(length) AS length_avg
+FROM film
+GROUP BY rating;
+
+-- Encuentra el nombre y apellido de los actores que aparecen en la película con title "Indian Love".
+SELECT a.first_name, a.last_name
+FROM actor a
+LEFT JOIN film_actor fa ON fa.actor_id = a.actor_id
+LEFT JOIN film f ON f.film_id = fa.film_id
+WHERE title = "Indian Love";
+
+-- Muestra el título de todas las películas que contengan la palabra "dog" o "cat" en su descripción.
+SELECT title, description
+FROM film
+WHERE description LIKE "%dog%" OR "%cat%";
+
+-- Encuentra el título de todas las películas que fueron lanzadas entre el añoo 2005 y 2010.
+SELECT title, release_year
+FROM film
+WHERE release_year BETWEEN 2005 AND 2010
+ORDER BY release_year;
