e83b6649783e181ce9626dfdc3edddd2d11dae24
Beatriz San José <beatrizsanjose.j@gmail.com>
2026-05-20 19:46:26 +0200

    Añadidas algunas consultas en ejercicio 1

--- ejercicio-1-api.sql
+++ ejercicio-1-api.sql
@@ -10,52 +10,71 @@ adultos BOOLEAN NOT NULL,
 subtitulos VARCHAR (100) 
 );
 
--- Cuántas películas tienen una duración superior a 120 minutos?
+-- 1. Cuántas películas tienen una duración superior a 120 minutos?
 SELECT *
 FROM peliculas
 WHERE duracion >120
 ORDER BY duracion;
 
---  Cuántas películas incluyen subtítulos en español?
+SELECT COUNT(*) AS peliculas_duracion_mas120
+FROM peliculas
+WHERE duracion >120;
+
+--  2.Cuántas películas incluyen subtítulos en español?
+SELECT COUNT(*) AS total_subtitulos_es
+FROM peliculas
+WHERE subtitulos LIKE '%es%';
+
 SELECT *
 FROM peliculas
 WHERE subtitulos LIKE '%es%';
 
---  Cuántas películas tienen contenido adulto?
+--  3.Cuántas películas tienen contenido adulto?
 SELECT *
 FROM peliculas
 WHERE adultos = True;
 
---  Cuál es la película más antigua registrada en la base de datos?
+SELECT COUNT(*) AS pelis_contenido_adulto
+FROM peliculas
+WHERE adultos = True;
+
+--  4.Cuál es la película más antigua registrada en la base de datos?
 SELECT *
 FROM peliculas
 ORDER BY anio ASC
 LIMIT 1;
 
--- Muestra el promedio de duración de las películas agrupado por género.
+SELECT *
+FROM peliculas
+WHERE anio = (
+SELECT MIN(anio)
+FROM peliculas);
+
+-- 5.Muestra el promedio de duración de las películas agrupado por género.
 SELECT genero, AVG(duracion)
 FROM peliculas
 GROUP BY genero;
 
---  Cuántas películas por año se han registrado en la base de datos? Ordena de mayor a menor.
+--  6.Cuántas películas por año se han registrado en la base de datos? Ordena de mayor a menor.
 SELECT anio, COUNT(anio) AS peliculas_por_anio
 FROM peliculas
 GROUP BY anio
 ORDER BY anio DESC;
 
---  Cuál es el año con más películas en la base de datos
+--  7.Cuál es el año con más películas en la base de datos
 SELECT anio, COUNT(anio) AS peliculas_por_anio
 FROM peliculas
 GROUP BY anio
 ORDER BY peliculas_por_anio DESC
-LIMIT 1;
+LIMIT 2;
 
--- Obtén un listado de todos los géneros y cuántas películas corresponden a cada uno.
+
+-- 8. Obtén un listado de todos los géneros y cuántas películas corresponden a cada uno.
 SELECT genero, COUNT(titulo) AS peliculas_por_genero
 FROM peliculas
 GROUP BY genero;
 
--- Muestra todas las películas cuyo título contenga la palabra "Godfather" (puedes usar cualquier palabra)
+-- 9.Muestra todas las películas cuyo título contenga la palabra "Godfather" (puedes usar cualquier palabra)
 SELECT *
 FROM peliculas
 WHERE titulo LIKE "%Godfather%";
